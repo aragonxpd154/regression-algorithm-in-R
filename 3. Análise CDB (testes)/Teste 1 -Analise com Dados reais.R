812250dfeb19915f2120bac956ab487b4ff57591
aragonxpd154 <marcos.silvabl@live.com>
2023-01-04 13:33:40 -0300

    Script de analíse de CDB (testes com dados reais)

---
+++ 3. Análise CDB (testes)/Teste 1 -Analise com Dados reais.R
@@ -0,0 +1,11 @@
+# Carregar os dados de investimentos de CDB
+cdb <- read.csv("cdb.csv")
+
+# Ordenar os dados pelo retorno de investimento
+cdb_ordenado <- cdb[order(cdb$retorno, decreasing = TRUE),]
+
+# Selecionar o investimento de CDB com o maior retorno
+investimento_mais_rentavel <- head(cdb_ordenado, 1)
+
+# Imprimir o resultado
+print(investimento_mais_rentavel)
