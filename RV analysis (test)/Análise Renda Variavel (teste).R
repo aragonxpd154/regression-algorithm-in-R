b780677a1eb987857124468d8c4d1b6e030c425d
Marcos (Obel) <marcos.silvabl@live.com>
2023-02-07 11:03:34 +0000

    Novos script

---
+++ RV analysis (test)/Análise Renda Variavel (teste).R
@@ -0,0 +1,19 @@
+# Carregar biblioteca tidyr para manipulação de dados
+library(tidyr)
+
+# Carregar dados do arquivo CSV para o data frame "stocks"
+stocks <- read.csv("dados_acoes.csv")
+
+# Transformar data em formato de data
+stocks$date <- as.Date(stocks$date, format = "%Y-%m-%d")
+
+# Agrupar dados por símbolo de ação e data
+stocks_grouped <- stocks %>% 
+  group_by(symbol, date) %>% 
+  summarise(mean_price = mean(price))
+
+# Plotar gráfico de linha para a média de preços por símbolo de ação e data
+ggplot(stocks_grouped, aes(x = date, y = mean_price, color = symbol)) +
+  geom_line() +
+  ggtitle("Gráfico de Preços Médios por Símbolo de Ação e Data")
+
